03f5cdde3329c40ef78aaafc78ef8755e7c2538d
akagaeng <akagaeng@gmail.com>
2018-01-05 17:03:36 +0900

    update file directory

--- MSP-2014/code/sol-10.c
+++ MSP-2014/code/sol-10.c
@@ -13,7 +13,7 @@ int main(int argc, char *argv[]) {
   printf("MSP 노래방에 오신 것을 환영합니다!\n\n");
   printf("번호\t가수 - 노래 제목\n");
 
-  fp = fopen("노래방.txt", "r");
+  fp = fopen("files/노래방.txt", "r");
 
   for (i = 0; feof(fp) == 0; i++) {
     fgets(str[i], sizeof(str[i]), fp);
@@ -33,7 +33,7 @@ int main(int argc, char *argv[]) {
     printf("\n\n(0번을 누르면 노래방 프로그램이 종료됩니다.)\n");
     printf("노래 번호를 입력해주세요 :");
     scanf("%d", &input);
-    
+
     if (input >= 1 && input <= cnt) { //번호에 있는 숫자 누른 경우
       for (i = input - 1, j = 0; str[i][j] != '\t'; j++) {
       } //탭있는곳까지 위치 구함
