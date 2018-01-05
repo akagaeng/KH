9eb77277a4aa78520e5f074d21bd276cca37f0fd
akagaeng <akagaeng@gmail.com>
2018-01-05 16:58:51 +0900

    indent update

--- MSP-2014/code/sol-6.c
+++ MSP-2014/code/sol-6.c
@@ -7,10 +7,13 @@ int main(int argc, char *argv[]) {
   FILE *fp2;
   int jan_go = 100000, juk_lip = 5000, pay = 0, pay_back = 0, use_juk_lip = 0;
   char ch = 0;
+
   fp1 = fopen("통장잔고.txt", "w+b");
   fp2 = fopen("적립금.txt", "w+b");
+
   fprintf(fp1, "%d", jan_go);
   fprintf(fp2, "%d", juk_lip);
+  
   while ((jan_go != 0)) {
     printf("현재 통장 잔고는 %d원이고, 적립금은 %d원 입니다.\n", jan_go, juk_lip);
     printf("구매하실 물건의 가격을 입력하세요: ");
@@ -21,6 +24,7 @@ int main(int argc, char *argv[]) {
       printf("적립금을 사용하시겠습니까? (Y/N) : ");
       scanf("%c", &ch);
       switch (ch) {
+ 
       case 'y':
       case 'Y':
         while (1) {
@@ -43,6 +47,7 @@ int main(int argc, char *argv[]) {
           } // end else
         }   // end while
         break;
+
       case 'n':
       case 'N':
         printf("%d원 적립되었습니다.\n", pay_back);
@@ -50,10 +55,13 @@ int main(int argc, char *argv[]) {
         jan_go = jan_go - pay;
         break;
       } // end switch
+
       fclose(fp1);
       fclose(fp2);
+
       fp1 = fopen("통장잔고.txt", "w+b");
       fp2 = fopen("적립금.txt", "w+b");
+      
       fprintf(fp1, "%d", jan_go);
       fprintf(fp2, "%d", juk_lip);
       fclose(fp1);
