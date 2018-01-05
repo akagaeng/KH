b592c79caf5b5fe488b490241d5d26f3e61ed3bc
akagaeng <akagaeng@gmail.com>
2018-01-05 16:51:29 +0900

    indent update

--- MSP-2014/code/sol-3.c
+++ MSP-2014/code/sol-3.c
@@ -3,9 +3,11 @@
 int main() {
   char arr[1024] = {0,};
   int i = 0, flag = 0, cnt1 = 0, cnt2 = 0;
+
   while (flag != 1) {
     printf("괄호를 입력해주세요: ");
     scanf("%s", &arr);
+
     for (i = 0; arr[i] != '\0'; i++) {
       if (arr[i] != '(' && arr[i] != ')') {
         printf("잘못입력하셨습니다. ( or ) 만 입력해주세요. \n");
@@ -16,6 +18,7 @@ int main() {
       }
     } // end for
   }   // end while
+
   for (i = 0; arr[i] != '\0' && flag != 0; i++) {
     if (arr[i] == '(') {
       cnt1++;
@@ -28,6 +31,7 @@ int main() {
       break;
     }
   }
+  
   if ((cnt1 - cnt2) == 0) {
     printf("YES");
   } else {
