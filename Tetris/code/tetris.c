dc6221d334bbe2fe3167c6a80d052cb7b0faa620
akagaeng <akagaeng@gmail.com>
2018-01-08 16:36:14 +0900

    initial commit

--- Tetris/code/tetris.c
+++ Tetris/code/tetris.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 #include <time.h>
 
-char block[7][4][4][2] = {
+char block[7][4][4][2] = { // 7가지 블록, 4가지 회전, 4칸의 블록, y축/x축 을 저장
     {{{0, 0}, {-1, 0}, {1, 0}, {2, 0}},
      {{0, 0}, {0, -1}, {0, 1}, {0, 2}},
      {{0, 0}, {-2, 0}, {-1, 0}, {1, 0}},
@@ -40,22 +40,17 @@ char block[7][4][4][2] = {
      {{0, 0}, {0, 1}, {1, 0}, {-1, 0}}}, // block[6][][][] : E block
 };
 
-int x, y, b, r;
+int x, y, b, r; // 계속 변화시키면서 적용해야 하는 변수들은 전역변수 선언
+                // x: x축 위치, y: y축 위치, b: 블록의 종류(0~6까지 7가지 종류), r: 회전
 int b_next;
 int shadow_y;
-int strike_cnt;
+int strike_cnt; // 바닥이나 블록에 부딪힌 횟수
 int shadow_strike_cnt;
 int strike_or_not;
-int line_cnt[22] = {
-    0,
-};
-int board[23][17] = {
-    0,
-};
-int next_block_board[9][11] = {
-    0,
-};
-int gap_var = 0;
+int line_cnt[22] = {0,}; // 각 라인에서의 블록의 갯수 저장
+int board[23][17] = {0,}; // 테트리스의 배경 및 벽, 쌓인 블록 저장
+int next_block_board[9][11] = {0,};
+int gap_var = 0; // 스페이스바 눌렀을 때에는 벽에 한번만 부딪혀도 쌓이도록 조정할때 활용
 int game_cnt = 1;
 int score = 0;
 int line_score = 0;
@@ -64,24 +59,25 @@ int ch;
 int moved;
 char gameover;
 char yesno;
-int i, j, k, l;
+int i, j, k, l; // 함수 내에서 자주 쓰는 변수이므로 그냥 전역변수로 한번만 선언해줌
 int pause_cnt;
 int level_cnt;
 int down_delay;
 int time_start;
 int time_end;
 char game_msg[29];
+// tile[]배열에 0번은 배경, 1번은 쌓이기 전 블록, 2번은 벽, 3번은 쌓인 후 벽을 저장
 char *tile[] = {".", "#", "M", "@", " ", "*", "/"};
 
-void board_arr() {
+void board_arr() { // 보드 배열에 저장만 함
   for (i = 0; i < 23; i++) {
     for (j = 0; j < 17; j++) {
-      if (i >= 0 && i < 4 && j != 0 && j != 16) {
+      if (i >= 0 && i < 4 && j != 0 && j != 16) { // y축 마지막줄, x축 첫번째 & 마지막 줄은 벽(M)으로 셋팅
         board[i][j] = 4;
       } else if (i == 22 || j == 0 || j == 16) {
         board[i][j] = 2;
       } else {
-        board[i][j] = 0;
+        board[i][j] = 0; // 나머지는 ..으로 세팅(저장만 함)
       }
     }
   }
@@ -99,10 +95,10 @@ void next_block_board_arr() {
   }
 }
 
-void board_print() {
+void board_print() { // 보드 배열에 저장된 것을 출력만 함
   for (i = 0; i < 23; i++) {
     for (j = 0; j < 17; j++) {
-      move(1 + i, 5 + j);
+      move(1 + i, 5 + j); // 1,5만큼 이동시킨 상태에서 board[i][j]에 저장된 타일 출력시킴
       addstr(tile[board[i][j]]);
     }
   }
@@ -118,7 +114,7 @@ void board_print() {
 void showblock() {
   for (i = 0; i < 4; i++) {
     move(1 + block[b][r][i][0] + y, 5 + block[b][r][i][1] + x);
-    addstr(tile[1]);
+    addstr(tile[1]); // 기본세팅인 1,5이동후 x축, y축 이동한 만큼 위치에 블록 출력
   }
 }
 
@@ -169,7 +165,7 @@ void move_down() {
       board[y + block[b][r][2][0] + 1][x + block[b][r][2][1]] == 3 ||
       board[y + block[b][r][3][0] + 1][x + block[b][r][3][1]] == 3) {
 
-    else {
+    else { // 아래로 이동중에 벽(2)이나 쌓인블록(3)이 없으면 계속 y축 이동
       y++;
     }
   }
@@ -185,7 +181,7 @@ void move_down() {
         board[y + block[b][r][1][0] + 1][x + block[b][r][1][1]] == 3 ||
         board[y + block[b][r][2][0] + 1][x + block[b][r][2][1]] == 3 ||
         board[y + block[b][r][3][0] + 1][x + block[b][r][3][1]] == 3) {
-
+      // 아래로 이동중에 한칸 아랫쪽에 벽(2)이나 쌓인블록(3)이 있으면 strike_cnt 증가시킴
       strike_cnt++;
     }
   }
@@ -198,14 +194,17 @@ void move_down() {
       board[y + block[b][r][1][0]][x + block[b][r][1][1]] = 3;
       board[y + block[b][r][2][0]][x + block[b][r][2][1]] = 3;
       board[y + block[b][r][3][0]][x + block[b][r][3][1]] = 3;
-
+      // 바로 아랫줄에 블록이 있는 경우에 한번 더 아래 키를 누르면 쌓임
+      // 스페이스바 누른 때에는 한번만 벽or쌓인블록 만나면 블록 쌓으면 됨
+      // 이때에 y가 한칸 덜 증가하므로 gap_var를 하나 줄여줌으로써 ↓키 누를때와 구별해줌
       this_score = 100 score = score + this_score + line_score;
       if (high_score <= score) {
         high_score = score;
       }
+      // 부딪힌 후 새로운 블록 위치 및 모양을 셋팅해줌
       x = 8;
       y = 3;
-      strike_cnt = 0;
+      strike_cnt = 0; // 부딪힌 횟수를 다시 체크해야 하므로 0으로 초기화
       strike_or_not = 1;
     }
   }
@@ -239,7 +238,7 @@ void move_down() {
     }
 
     else {
-      x--;
+      x--; // 왼쪽으로 이동중에 벽(2)이나 쌓인블록(3)이 없으면 계속 왼쪽으로 이동
     }
   }
 
@@ -257,7 +256,7 @@ void move_down() {
     }
 
     else {
-      x++;
+      x++; // 오른쪽으로 이동중에 벽(2)이나 쌓인블록(3)이 없으면 계속 오른쪽으로 이동
     }
   }
 
@@ -283,7 +282,7 @@ void move_down() {
     }
 
     else {
-      r = (r + 1) % 4;
+      r = (r + 1) % 4; // 회전 방향의 다음 회전시에 벽이나 블록에 부딪치지 않으면 회전
     }
   }
 
@@ -293,10 +292,10 @@ void move_down() {
     for (i = 0; remove_line = 0; i < 22; i++) {
       line_cnt[i] = 0;
       for (j = 0; j < 16; j++) {
-        if (board[i][j] == 3) {
+        if (board[i][j] == 3) { //각 라인의 블록수를 배열에 저장
           line_cnt[i]++;
         }
-   if( line_cnt[i]==15{
+   if( line_cnt[i]==15{ // 0부터 증가시킨 어떤 라인의 블록수가 15개(한줄 전체)이면
           level_cnt++;
           remove_line++;
           if (level_cnt % 2 == 0) {
@@ -356,19 +355,19 @@ void move_down() {
     clear();
     score = 0;
     ch = 0;
-    x = 8;
-    y = 3;
+    x = 8; // 가운데
+    y = 3; // 회전해도 보드 밖으로 나가지 않도록
     shadow_y = 3;
     srand(time(NULL));
-    b = rand() % 7;
+    b = rand() % 7; // 7가지 중 랜덤한 모양 나오도록
     b_next = rand() % 7;
-    r = 0;
-    strike_cnt = 0;
+    r = 0; // 회전 방향 기본셋팅 -> 이후 반시계방향으로 회전
+    strike_cnt = 0; // 부딪힌 횟수 0회로 초기화
     shadow_strike_cnt = 0;
     pause_cnt = 0;
     level_cnt = 0;
     down_delay = 100;
-    board_arr();
+    board_arr(); // 보드배열 기본세팅
     next_block_board_arr();
     strike_or_not = 0; // 0: not strike 1:strike
     gap_var = 0;
@@ -457,13 +456,13 @@ void move_down() {
           move_right();
           refresh();
           break;
-        case ' ':
-          while (strike_cnt != 1) {
-            move_down();
-            strike_block();
+        case ' ': // space bar
+          while (strike_cnt != 1) { // 부딪친횟수 1회될때까지
+            move_down(); // 아래로 움직이며
+            strike_block(); // 부딪히는지 체크
           }
           strike_cnt++;
-          gap_var = 0;
+          gap_var = 0; // 스페이스바 눌렀을때 그 줄에 블록 쌓이도록 처리
           striked_block();
           gap_var = -1;
           refresh();
@@ -494,7 +493,7 @@ void move_down() {
         } // end switch(ch)
       }   // end else
 
-      line_check();
+      line_check(); // 지워지는 라인 있는지 체크
       refresh();
 
       /*	GAME OVER CHECK		*/
